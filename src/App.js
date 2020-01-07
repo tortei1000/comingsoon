f65c14d2a636a2c010237efec371ff276bd4223b
tiago maffei <tiagomaffei@tiagos-mbp.lan>
2020-01-07 14:37:12 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import logo from "./sharebucks_logo_gray.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} alt="sharebucks icon" />
+      <h1>Coming</h1>
+      <h2>2020</h2>
     </div>
   );
 }
