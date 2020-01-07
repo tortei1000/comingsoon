e7aab8385901b7d6b81ec1bad9bb15b83a86ee90
tiago maffei <tiagomaffei@tiagos-mbp.lan>
2020-01-07 16:36:51 -0700

    server

--- server/index.js
+++ server/index.js
@@ -9,10 +9,11 @@ const path = require('path'); // Usually moved to the start of file
 
 app.use(express.json())
 
+app.use( express.static( `${__dirname}/../build` ) );
 
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../build/index.html'));
-});
+// app.get('*', (req, res)=>{
+//     res.sendFile(path.join(__dirname, '../build/index.html'));
+// });
 
 massive(CONNECTION_STRING).then((database) => {
   app.set('db', database)
